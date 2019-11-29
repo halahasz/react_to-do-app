7fafa95f9b75b48b143a45a44d3f0947d999585e
Halina Bałys <halina.balys@ptrdigital.pl>
2019-11-29 11:48:37 +0100

    Updates

---
+++ precache-manifest.a7c0de563d669100aa86118943d84e1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9487fb15f98345be1487bf7ed99af4",
+    "url": "/react_to-do-app/index.html"
+  },
+  {
+    "revision": "df0000b67e3539dcf67c",
+    "url": "/react_to-do-app/static/css/main.990a5408.chunk.css"
+  },
+  {
+    "revision": "afffa8ae4c5e266a6942",
+    "url": "/react_to-do-app/static/js/2.c7d96675.chunk.js"
+  },
+  {
+    "revision": "df0000b67e3539dcf67c",
+    "url": "/react_to-do-app/static/js/main.cb3c2aa2.chunk.js"
+  },
+  {
+    "revision": "46ee4ea9b504466eca8c",
+    "url": "/react_to-do-app/static/js/runtime-main.7c129ebc.js"
+  }
+]);
